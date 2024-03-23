371733a7b51b5c9fdf18072cdcc293d8b5f8e731
Prashant Kumar <prashant.aman9@gmail.com>
2024-03-23 17:22:25 +0530

    Add and setup eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,45 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true,
+  },
   extends: [
-    'eslint:recommended',
+    'airbnb',
+    'airbnb/hooks',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
   rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
     ],
+    semi: 0,
+    'import/no-extraneous-dependencies': 0,
+    'react/react-in-jsx-scope': 0,
+    'linebreak-style': 0,
+    'react/prop-types': 0,
+    'react/jsx-props-no-spreading': 0,
+    'comma-dangle': 0,
+    'react-hooks/exhaustive-deps': 0,
+    'react/no-array-index-key': 0,
   },
 }
