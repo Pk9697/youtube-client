491956befaa1ef541304138c508702b3176391f2
Prashant Kumar <prashant.aman9@gmail.com>
2024-03-26 17:14:13 +0530

    Add react-router-dom and configure routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import Login from './pages/Login'
-import Register from './pages/Register'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import * as ROUTES from '@/constants/routes'
+import Login from '@/pages/Login'
+import Register from '@/pages/Register'
 
 function App() {
   return (
-    <div className="app">
-      <Login />
-      {/* <Register /> */}
-    </div>
+    <Router>
+      <Routes>
+        <Route path={ROUTES.HOME} element={<div>Home</div>} />
+        <Route path={ROUTES.LOGIN} element={<Login />} />
+        <Route path={ROUTES.REGISTER} element={<Register />} />
+      </Routes>
+    </Router>
   )
 }
 
