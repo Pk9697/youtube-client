dde6ca8f80b58a5317686c2d9cf098ddd6e525ea
Prashant Kumar <prashant.aman9@gmail.com>
2024-04-24 16:41:55 +0530

    Remove isChecked derived state from PlaylistCheckboxContainer

--- src/features/playlist/layouts/PlaylistCheckboxContainer.jsx
+++ src/features/playlist/layouts/PlaylistCheckboxContainer.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { EarthIcon, EarthLockIcon } from 'lucide-react'
 import Playlist from '../components/Playlist'
@@ -16,9 +15,7 @@ function PlaylistCheckboxContainer({
 }) {
   const dispatch = useDispatch()
   const { accessToken } = useSelector((state) => state.auth)
-  const [isChecked, setIsChecked] = useState(
-    videos.some((video) => video._id === videoId)
-  )
+  const isChecked = videos.some((video) => video._id === videoId)
 
   const handleCheckboxClick = () => {
     if (isChecked) {
@@ -26,8 +23,6 @@ function PlaylistCheckboxContainer({
     } else {
       dispatch(addVideoToPlaylist({ accessToken, playlistId, videoId }))
     }
-
-    setIsChecked((prev) => !prev)
   }
 
   return (
