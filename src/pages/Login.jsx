c05782a9ee5ef2d0f5b23a54aa360d8177338fbd
Prashant Kumar <prashant.aman9@gmail.com>
2024-05-15 19:51:44 +0530

    Update Login and Register page for loading state

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,11 +2,12 @@ import { Navigate, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { ROUTES } from '@/data/constants'
 import { Container, Form, useLogin } from '@/features/authentication'
-import Loader from '@/components/Loader'
+import useApp from '@/app/useApp'
 
 function Login() {
+  const { isLoading: isLoadingLogin } = useApp('auth/login')
   const { email, password, handleChange, handleSubmit } = useLogin()
-  const { inProgress, isLoggedIn } = useSelector((state) => state.auth)
+  const { isLoggedIn } = useSelector((state) => state.auth)
   const navigate = useNavigate()
 
   if (isLoggedIn) {
@@ -15,60 +16,58 @@ function Login() {
 
   return (
     <Container>
-      <Loader inProgress={inProgress}>
-        <Form onSubmit={handleSubmit}>
-          <Form.Header>
-            <Form.Title>Login</Form.Title>
-            <Form.Description>
-              Enter your email below to login to your account
-            </Form.Description>
-          </Form.Header>
-          <Form.Content>
-            <Form.GridGroup>
-              <Form.InputContainer>
-                <Form.Label htmlFor="email">Email</Form.Label>
-                <Form.Input
-                  value={email}
-                  onChange={handleChange}
-                  name="email"
-                  id="email"
-                  type="email"
-                  autoComplete="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </Form.InputContainer>
-              <Form.InputContainer>
-                <Form.FlexRow className="justify-between">
-                  <Form.Label htmlFor="password">Password</Form.Label>
-                  <Form.Link>Forgot your password?</Form.Link>
-                </Form.FlexRow>
-                <Form.Input
-                  value={password}
-                  onChange={handleChange}
-                  name="password"
-                  id="password"
-                  type="password"
-                  autoComplete="password"
-                  required
-                />
-              </Form.InputContainer>
-              <Form.Button disabled={inProgress} type="submit">
-                Login
-              </Form.Button>
-              <Form.Button disabled={inProgress} variant="outline">
-                Login with Google
-              </Form.Button>
-            </Form.GridGroup>
-            <Form.Text>
-              Don&apos;t have an account?{' '}
-              <Form.Link onClick={() => navigate(ROUTES.REGISTER)}>
-                Register
-              </Form.Link>
-            </Form.Text>
-          </Form.Content>
-        </Form>
-      </Loader>
+      <Form onSubmit={handleSubmit}>
+        <Form.Header>
+          <Form.Title>Login</Form.Title>
+          <Form.Description>
+            Enter your email below to login to your account
+          </Form.Description>
+        </Form.Header>
+        <Form.Content>
+          <Form.GridGroup>
+            <Form.InputContainer>
+              <Form.Label htmlFor="email">Email</Form.Label>
+              <Form.Input
+                value={email}
+                onChange={handleChange}
+                name="email"
+                id="email"
+                type="email"
+                autoComplete="email"
+                placeholder="m@example.com"
+                required
+              />
+            </Form.InputContainer>
+            <Form.InputContainer>
+              <Form.FlexRow className="justify-between">
+                <Form.Label htmlFor="password">Password</Form.Label>
+                <Form.Link>Forgot your password?</Form.Link>
+              </Form.FlexRow>
+              <Form.Input
+                value={password}
+                onChange={handleChange}
+                name="password"
+                id="password"
+                type="password"
+                autoComplete="password"
+                required
+              />
+            </Form.InputContainer>
+            <Form.Button disabled={isLoadingLogin} type="submit">
+              Login
+            </Form.Button>
+            <Form.Button disabled={isLoadingLogin} variant="outline">
+              Login with Google
+            </Form.Button>
+          </Form.GridGroup>
+          <Form.Text>
+            Don&apos;t have an account?{' '}
+            <Form.Link onClick={() => navigate(ROUTES.REGISTER)}>
+              Register
+            </Form.Link>
+          </Form.Text>
+        </Form.Content>
+      </Form>
     </Container>
   )
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,12 +3,14 @@ import { Navigate, useNavigate } from 'react-router-dom'
 import { Container, Form } from '@/features/authentication'
 import useRegister from '@/features/authentication/hooks/useRegister'
 import { ROUTES } from '@/data/constants'
-import Loader from '@/components/Loader'
+import useApp from '@/app/useApp'
 
 function Register() {
+  const { isLoading: isLoadingRegister } = useApp('auth/register')
+
   const { fullName, email, userName, password, handleChange, handleSubmit } =
     useRegister()
-  const { user, inProgress, isLoggedIn } = useSelector((state) => state.auth)
+  const { user, isLoggedIn } = useSelector((state) => state.auth)
   const navigate = useNavigate()
 
   if (isLoggedIn) {
@@ -23,106 +25,102 @@ function Register() {
 
   return (
     <Container>
-      <Loader inProgress={inProgress}>
-        <Form onSubmit={handleSubmit}>
-          <Form.Header>
-            <Form.Title>Register</Form.Title>
-            <Form.Description>
-              Enter your information to create an account
-            </Form.Description>
-          </Form.Header>
-          <Form.Content>
-            <Form.GridGroup>
-              <Form.GridGroup className="grid-cols-2">
-                <Form.InputContainer>
-                  <Form.Label htmlFor="fullName">Full name</Form.Label>
-                  <Form.Input
-                    value={fullName}
-                    onChange={handleChange}
-                    name="fullName"
-                    id="fullName"
-                    autoComplete="fullName"
-                    placeholder="Max Robinson"
-                    required
-                  />
-                </Form.InputContainer>
-                <Form.InputContainer>
-                  <Form.Label htmlFor="userName">Username</Form.Label>
-                  <Form.Input
-                    value={userName}
-                    onChange={handleChange}
-                    name="userName"
-                    id="userName"
-                    autoComplete="username"
-                    placeholder="robin123"
-                    required
-                  />
-                </Form.InputContainer>
-              </Form.GridGroup>
+      <Form onSubmit={handleSubmit}>
+        <Form.Header>
+          <Form.Title>Register</Form.Title>
+          <Form.Description>
+            Enter your information to create an account
+          </Form.Description>
+        </Form.Header>
+        <Form.Content>
+          <Form.GridGroup>
+            <Form.GridGroup className="grid-cols-2">
               <Form.InputContainer>
-                <Form.Label htmlFor="avatar">Avatar</Form.Label>
+                <Form.Label htmlFor="fullName">Full name</Form.Label>
                 <Form.Input
+                  value={fullName}
                   onChange={handleChange}
-                  name="avatar"
-                  id="avatar"
-                  type="file"
-                  required
-                  className="file:text-primary"
-                  accept="image/*"
-                />
-              </Form.InputContainer>
-              <Form.InputContainer>
-                <Form.Label htmlFor="coverImage">Cover Image</Form.Label>
-                <Form.Input
-                  onChange={handleChange}
-                  name="coverImage"
-                  id="coverImage"
-                  type="file"
-                  className="file:text-primary"
-                  accept="image/*"
-                />
-              </Form.InputContainer>
-              <Form.InputContainer>
-                <Form.Label htmlFor="email">Email</Form.Label>
-                <Form.Input
-                  value={email}
-                  onChange={handleChange}
-                  name="email"
-                  id="email"
-                  type="email"
-                  autoComplete="email"
-                  placeholder="m@example.com"
+                  name="fullName"
+                  id="fullName"
+                  autoComplete="fullName"
+                  placeholder="Max Robinson"
                   required
                 />
               </Form.InputContainer>
               <Form.InputContainer>
-                <Form.Label htmlFor="password">Password</Form.Label>
+                <Form.Label htmlFor="userName">Username</Form.Label>
                 <Form.Input
-                  value={password}
+                  value={userName}
                   onChange={handleChange}
-                  name="password"
-                  id="password"
-                  type="password"
-                  autoComplete="password"
+                  name="userName"
+                  id="userName"
+                  autoComplete="username"
+                  placeholder="robin123"
                   required
                 />
               </Form.InputContainer>
-              <Form.Button disabled={inProgress} type="submit">
-                Register
-              </Form.Button>
-              <Form.Button disabled={inProgress} variant="outline">
-                Register with Google
-              </Form.Button>
             </Form.GridGroup>
-            <Form.Text>
-              Already have an account?{' '}
-              <Form.Link onClick={() => navigate(ROUTES.LOGIN)}>
-                Login
-              </Form.Link>
-            </Form.Text>
-          </Form.Content>
-        </Form>
-      </Loader>
+            <Form.InputContainer>
+              <Form.Label htmlFor="avatar">Avatar</Form.Label>
+              <Form.Input
+                onChange={handleChange}
+                name="avatar"
+                id="avatar"
+                type="file"
+                required
+                className="file:text-primary"
+                accept="image/*"
+              />
+            </Form.InputContainer>
+            <Form.InputContainer>
+              <Form.Label htmlFor="coverImage">Cover Image</Form.Label>
+              <Form.Input
+                onChange={handleChange}
+                name="coverImage"
+                id="coverImage"
+                type="file"
+                className="file:text-primary"
+                accept="image/*"
+              />
+            </Form.InputContainer>
+            <Form.InputContainer>
+              <Form.Label htmlFor="email">Email</Form.Label>
+              <Form.Input
+                value={email}
+                onChange={handleChange}
+                name="email"
+                id="email"
+                type="email"
+                autoComplete="email"
+                placeholder="m@example.com"
+                required
+              />
+            </Form.InputContainer>
+            <Form.InputContainer>
+              <Form.Label htmlFor="password">Password</Form.Label>
+              <Form.Input
+                value={password}
+                onChange={handleChange}
+                name="password"
+                id="password"
+                type="password"
+                autoComplete="password"
+                required
+              />
+            </Form.InputContainer>
+            <Form.Button disabled={isLoadingRegister} type="submit">
+              Register
+            </Form.Button>
+            <Form.Button disabled={isLoadingRegister} variant="outline">
+              Register with Google
+            </Form.Button>
+          </Form.GridGroup>
+          <Form.Text>
+            Already have an account?{' '}
+            <Form.Link onClick={() => navigate(ROUTES.LOGIN)}>Login</Form.Link>
+          </Form.Text>
+        </Form.Content>
+      </Form>
     </Container>
   )
 }
